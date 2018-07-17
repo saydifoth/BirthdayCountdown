0f403b33a2f2ef4334d57c46f8d78a167b8271fc
saydifoth <saydimccall@gmail.com>
2018-07-17 11:53:18 -0600

    made application fumctional

--- src/components/app.js
+++ src/components/app.js
@@ -5,39 +5,126 @@ import Clock from './clock';
 import ChangeDate from './changeDate';
 import LargeText from './largeText';
 
+import moment from "moment";
+
 export default class App extends Component {
+  constructor(props) {
+    super(props);
 
-constructor(props) {
-  super(props)
+    this.timer = 0;
 
-  this.state= {
-    active: false
-  }
+    this.state = {
+      active: false,
+      startDate: moment (),
+      timeRemaining: {
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0
+    },
+    age: 0
+  };
+
+  this.handleGenerate = this.handleGenerate.bind(this);
 }
+
+handleChange = function(date) {
+  console.log('APP JS HANDLE CHANGE', date._d);
+  clearInterval(this.timer);
+this.setState({
+  startDate: date
+});
+}.bind(this);
+
+handleGenerate = function() {
+  this.setState({ active: true });
+
+  var bday = this.state.startDate.toDate();
+  var today = new Date();
+  var currentMonth = today.getMonth();
+  var birthMonth = bday.getMonth();
+
+  var timeBetween = today.getTime() - bday.getTime();
+  var daysOld = Math.floor(timeBetween / (1000 * 60 * 60 * 24))
+  var age = Number((daysOld/365).toFixed(0));
+  this.setState({age, 
+    active:true
+  })
+
+  if(birthMonth > currentMonth) {
+    bday.setFullYear(today.getFullYear())
+  } else if(birthMonth < currentMonth) {
+    bday.setFullYear(today.getFullYear() +1)
+  }else if(birthMonth == currentMonth) {
+    var currentDay = today.getDate();
+    var birthDay = bday.getDate();
+
+    if(birthDay > currentDay) {
+      bday.setFullYear(today.getFullYear())
+    }
+    if(birthDay <= currentDay) {
+      bday.setFullYear(today.getFullYear() +1)
+    }
+  }
+  
+    var countDownDate = bday.getTime();
+
+   this.timer = setInterval(function() {
+
+    var now = today.getTime();
+
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    const time = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+    const timeRemaining = {
+      days,
+      hours,
+      minutes,
+      seconds
+    };
+    this.setState({ timeRemaining });
+
+    if (distance < 0) {
+      clearInterval(this.timer);
+    }
+
+  }.bind(this), 1000);
+}.bind(this);
+
 
 renderItems = function() {
   if(this.state.active) {
     return [
-      <Clock/>,
-      ChangeDate('Change Date', () => this.setState({active: false })),
+      <Clock timeRemaining={this.state.timeRemaining} />,
+      ChangeDate('Change Date', () => this.setState({ active: false })),
       LargeText('04/03'),
-      <label className="grid__remaining">Remaining until your 21st Birthday</label>
-    ]
-  }else {
+      <label className="grid__remaining">
+      Remaining until you turn {this.state.age}
+      </label>
+    ];
+  } else {
     return [ 
-      <Picker/>,
-      Button('Generate Countdown', () => this.setState({ active: true}))
+      <Picker 
+      startDate={this.state.startDate} 
+      callback={date => this.handleChange(date)}
+      />,
+      Button('Generate Countdown', () => this.handleGenerate())
     ] 
   }
 }.bind(this)
 
   render() {
-
     return (
       <div className="grid">
         <h1 className="grid__title">Birthday Countdown</h1>
 
-        <div className="grid__skew-dark-one-rectangle"></div>
         <div className="grid__skew-dark-two"></div>
         <div className="grid__skew-dark-three"></div>
 
@@ -54,4 +141,5 @@ renderItems = function() {
       </div>
     );
   }
+
 }
--- src/components/picker.js
+++ src/components/picker.js
@@ -1,39 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+import DatePicker from "react-datepicker";
+import moment from "moment";
 
 import 'react-datepicker/dist/react-datepicker.css';
 
-import DatePicker from 'react-datepicker';
-import moment from 'moment';
-
 class Picker extends Component {
 
-  constructor (props) {
-    super(props)
-    this.state = {
-      startDate: moment()
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
- 
-  handleChange(date) {
-      console.log('trying to change date for', date._d);
-    this.setState({
-      startDate: date
-    });
-  }
+  handleChange = function(date) {
+    this.props.callback(date)
+  }.bind(this);
     
 render() {
  return (
     <div className="picker">
         <DatePicker
-            selected={this.state.startDate}
+            selected={this.props.startDate}
             onChange={this.handleChange}
                 
          />
      </div>
-    )
-            
-        
+    );    
     }
 }
 
